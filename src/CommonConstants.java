5613c153cd218954c8013ea84b12342d89cfda50
Lavanyapoojary <lavapoojary17@gmail.com>
2025-07-23 16:52:17 +0530

    Add basic Hangman GUI structure

---
+++ src/CommonConstants.java
@@ -0,0 +1,5 @@
+import java.awt.*;
+public class CommonConstants {
+    //size config
+    public static final Dimension FRAME_SIZE = new Dimension(540,760);
+}
---
+++ src/Game.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Game {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable(){
+            @Override
+            public void run() {
+                new Hangman().setVisible(true);
+            }
+        });
+    }
+}
+
+
+
