6096e7d916dce0c9e0b0461b7fdb6a2701673290
Lavanyapoojary <lavapoojary17@gmail.com>
2025-07-24 19:52:39 +0530

    fix(worddb): correct declaration and logic for category and word loading

--- src/CommonConstants.java
+++ src/CommonConstants.java
@@ -1,5 +1,7 @@
 import java.awt.*;
 public class CommonConstants {
+    //file paths
+    public static final String DATA_PATH = "resources/data.txt";
     //size config
     public static final Dimension FRAME_SIZE = new Dimension(540,760);
 }
